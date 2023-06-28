bbd8c2fac38efe9e0241541579a742adf0101045
calmmin <mj3242@naver.com>
2023-06-28 20:33:14 +0900

    feat : HashingUtil 클래스 정의
    Id, PW 값을 해싱하는 메서드를 제공하는 util 클래스 정의

---
+++ Cockpybara/src/main/java/Alchole_free/Cockpybara/controller/member/util/HashingUtil.java
@@ -0,0 +1,34 @@
+package Alchole_free.Cockpybara.controller.member.util;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class HashingUtil {
+
+    private static final String HASH_ALGORITHM="SHA-256";
+
+    public static String hashPassword(String value){
+        try{
+            MessageDigest digest = MessageDigest.getInstance(HASH_ALGORITHM);
+
+            byte[] encodedHash = digest.digest(value.getBytes(StandardCharsets.UTF_8));
+
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : encodedHash) {
+                String hex=Integer.toHexString(0xff & b);
+                if(hex.length()==1){
+                    hexString.append('0');
+                }
+                hexString.append(hex);
+            }
+
+            return hexString.toString();
+        }catch(NoSuchAlgorithmException e){
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+}
