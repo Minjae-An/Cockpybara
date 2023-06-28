2588c7d835dea624e9ed2721ec0dd2d1fa6445b4
calmmin <mj3242@naver.com>
2023-06-28 15:17:05 +0900

    feat : MemberRepository 클래스 정의(#5)
    Member 엔티티의 데이터 접근 계층 MemberRepository 클래스 정의
    JpaRepository 인터페이스를 활용하여 구현하였으며 이메일과 패스워드를
    통해 Member를 조회할 수 있는 findByEmailAndPassword 메서드를 로그인시
    활용 가능하도록 별도로 구현하였다.

---
+++ Cockpybara/src/main/java/Alchole_free/Cockpybara/repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package Alchole_free.Cockpybara.repository;
+
+import Alchole_free.Cockpybara.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmailAndPassword(String email, String password);
+}
