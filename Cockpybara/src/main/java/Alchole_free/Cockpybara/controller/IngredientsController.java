79d44ba0eef9dff172cd9088a4d9587f98ad1882
jisu <comjisu0311@naver.com>
2023-06-29 11:40:13 +0900

    feat: IngredientsControllerr구현

--- Cockpybara/src/main/java/Alchole_free/Cockpybara/controller/IngredientsController.java
+++ Cockpybara/src/main/java/Alchole_free/Cockpybara/controller/IngredientsController.java
@@ -1,16 +1,26 @@
 package Alchole_free.Cockpybara.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import javax.validation.Valid;
+import Alchole_free.Cockpybra.service.IngredientsService;
+
 @RestController
+@RequestMapping("/cocktail")
 public class CocktailController {
     private final IngredientsService ingredientsService;
-    @GetMapping("cocktail/option-list")
-    //받아오는 부분이랑 되돌려주는 값 변경해야함
-    public String helloString(@PathVariable String id) {
 
-        return "id is : " + id;
+    @Autowired
+    public CocktailController(IngredientsService ingredientsService) {
+        this.ingredientsService = ingredientsService;
+    }
+
+    @GetMapping("/option-list")
+    public List<String> getFilteredIngredients() {
+        // 재료 필터링 작업을 수행하고, 필터링된 재료 리스트를 반환하는 서비스 메소드 호출
+        List<String> filteredIngredients = ingredientsService.getFilteredIngredients();
+
+        return filteredIngredients;
     }
 }
