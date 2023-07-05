a1f723a04f923aafeea54830701515f0e7173b99
soz1p <jeonsojin1363@hansung.ac.kr>
2023-07-05 11:57:09 +0900

    feat : 레시피 디테일 페이지 생성 CocktailDetail.js 생성

---
+++ Cockpybara/src/main/frontend/src/CocktailDetail.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+
+function CocktailDeatil() {
+
+    return (
+        <div className='recipeDetail'>
+            <h1>Cocktail Name</h1>
+        </div>
+    );
+}
+
+export default CocktailDeatil;
