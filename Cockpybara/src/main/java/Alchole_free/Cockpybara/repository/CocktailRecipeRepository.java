e0b8effcb04c969ce141c745da5e4c185b7c39f7
calmmin <mj3242@naver.com>
2023-07-11 21:30:36 +0900

    feat : CocktailRecipeRepository - findCocktailRecipeByName 메서드 추가(#11)
    이름으로 검색을 수행하는데 필요한 메서드, CocktailRecipeRepository 추가

--- Cockpybara/src/main/java/Alchole_free/Cockpybara/repository/CocktailRecipeRepository.java
+++ Cockpybara/src/main/java/Alchole_free/Cockpybara/repository/CocktailRecipeRepository.java
@@ -4,6 +4,9 @@ import Alchole_free.Cockpybara.domain.cocktail_recipe.CocktailRecipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CocktailRecipeRepository extends JpaRepository<CocktailRecipe, Long>{
+    Optional<CocktailRecipe> findCocktailRecipeByName(String name);
 }
