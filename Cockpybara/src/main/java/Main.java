974f168e90f7dd6599136d8dece73469525adbf1
jisu <comjisu0311@naver.com>
2023-07-11 15:23:55 +0900

    modify measure correction code, add ingredients.csv

--- Cockpybara/src/main/java/Main.java
+++ Cockpybara/src/main/java/Main.java
@@ -13,89 +13,50 @@ public class Main {
     private static HashMap<String, ArrayList<String>> map = new HashMap<>();
     private static HashSet<String> units = new HashSet<>();
     private static HashSet<String> glass = new HashSet<>();
+
     public static void main(String[] args) throws CsvValidationException, IOException {
         String csvFile = "src\\main\\resources\\all_drinks.csv"; // 읽을 CSV 파일의 경로
 
         //csv파일 수정 - 비슷한 단위들 단일화
         try (CSVReader reader = new CSVReader(new FileReader(csvFile))) {
-            String[] nextLine =  reader.readNext();
+            String[] nextLine = reader.readNext();
 
+            int rowNumber = 1; // 현재 행 번호 - 단위존재x 재료찾는 코드에 사용
             while ((nextLine = reader.readNext()) != null) {
                 glass.add(nextLine[7]);
                 for (int i = 9; i <= 23; i++) {
-
                     String ingredient = nextLine[i];
-                    if (ingredient == null || ingredient.trim().equals("")){
+                    if (ingredient == null || ingredient.trim().isEmpty()) {
                         continue;
                     }
-                    //25 - 39
 
                     String measure = nextLine[i + 16];
-                    if (measure.equals("\n")) continue;
-
-                    //단위 변경 코드 추가
-                    if (measure.contains("oz")) measure = "oz"; //온즈(1oz = 30ml)
-                    if (measure.contains("ml")) measure = "ml";
-                    if (measure.contains("tblsp")) measure = "tblsp "; //숟가락(큰술)
-                    if (measure.contains("tsp")) measure = "tsp"; //작은 숟가락
-                    if (measure.contains("cup")) measure = "cup";
-                    if (measure.contains("shot")) measure = "shot";
-                    if (measure.contains("cl")) measure = "cl"; //센티리터
-                    if (measure.contains("shot")) measure = "shot";
-                    if (measure.contains("inch")) measure = "inch";  //길이 inch
-                    if (measure.contains("glass")) measure = "glass";
-                    if (measure.contains("bottle ")) measure = "bottle ";
-                    if (measure.contains("lb")) measure = "lb"; //파운드(1lb = 0.45kg)
-                    if (measure.contains("gal")) measure = "gal";
-
-                    ArrayList<String> list;
-                    if ((list = map.get(ingredient)) != null) {
-
-                    }
-                    else {
-                        list = new ArrayList<>();
+                    if (measure.equals("\n") || measure.trim().isEmpty()) {
+                        printMissingIngredient(ingredient, rowNumber); // 단위가 존재하지 않는 재료 출력
+                        measure = "fill"; // 단위가 존재하지 않는 경우 "fill"로 변경
                     }
 
-                    int startIdx = measure.indexOf(" ");
-                    String unit;
-                    if (startIdx != -1) {
-                        unit = measure.substring(startIdx).trim();
-                        while (unit.contains("/")) {
-                            startIdx = measure.indexOf(" ", startIdx + 1);
-                            unit = measure.substring(startIdx).trim();
-                        }
-                    }
-                    else {
-                        unit = measure.trim();
-                    }
-                    if (!list.contains(unit)) {
-                        units.add(unit);
-                        list.add(unit);
+                    measure = normalizeUnit(measure); // 단위 통일화
+
+                    ArrayList<String> list = map.getOrDefault(ingredient, new ArrayList<>());
+                    if (!list.contains(measure)) {
+                        units.add(measure);
+                        list.add(measure);
                     }
                     map.put(ingredient, list);
                 }
-//                int i = 0;
-//                for (String cell : nextLine) {
-//                    System.out.print(i++ + " : " + cell + "\n");
-//                }
-//                System.out.println();
+                rowNumber++;
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        //시험용 코드 - 파일 잘 바뀌고 출력잘되는지 여기서 확인할것, 코드 추가해야함!
-        for (String s : glass) {
-            System.out.println(s + ",");
-        }
 
-        System.out.println(glass.size());
-
-        //위에 수정된 내용을 토대로 새로운 재료 ingredients.csv파일 생성
+        // 칵테일 CSV 파일에서 재료와 단위를 정제하여 새로운 CSV 파일 생성
         String outputCsvFile = "src/main/resources/all_ingredients.csv"; // 저장할 CSV 파일의 경로
 
         try (CSVWriter writer = new CSVWriter(new FileWriter(outputCsvFile))) {
             // 헤더 라인 작성
-            String[] header = { "Ingredient", "Unit" };
+            String[] header = {"Ingredient", "Unit"};
             writer.writeNext(header);
 
             // 데이터 작성
@@ -104,28 +65,108 @@ public class Main {
                 ArrayList<String> units = entry.getValue();
 
                 for (String unit : units) {
-                    String[] data = { ingredient, unit };
+                    String[] data = {ingredient, unit};
                     writer.writeNext(data);
                 }
             }
-
-            System.out.println("all_ingredients.csv 파일이 생성되었습니다.");
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        //
+        // 정제된 재료와 단위 출력
+        System.out.println("Unit List:");
+        for (String unit : units) {
+            System.out.println(unit);
+        }
+        System.out.println("Total Units: " + units.size());
 
-//        for (String s : units) {
-//            System.out.println(s + ",");
-//        }
-//
-//        System.out.println(units.size());
-
-//        for (Map.Entry<String, ArrayList<String>> en : map.entrySet()) {
-//            System.out.println(en);
-//        }
-
-//        System.out.println(map);
+        // 파운드를 그램으로 변환
+       // convertPoundToGram(outputCsvFile);
     }
+
+    private static void printMissingIngredient(String ingredient, int rowNumber) {
+        System.out.println("Missing Ingredient: " + ingredient + ", Row: " + rowNumber);
+    }
+
+    private static String normalizeUnit(String measure) {
+        measure = measure.toLowerCase().trim();
+
+        if (measure.contains("oz") || measure.contains("jigger")) {
+            return "oz";
+        }
+        else if (measure.contains("ml") || measure.contains("dl") || measure.contains("pint")) {
+            return "ml";
+        }
+        else if (measure.contains("tblsp")) {
+            return "tblsp";
+        }
+        else if (measure.contains("tsp") || measure.contains("spoon")) {
+            return "tsp";
+        }
+        else if (measure.contains("cup")) {
+            return "cup";
+        }
+        else if (measure.contains("shot")) {
+            return "shot";
+        }
+        else if (measure.contains("slice") || measure.contains("wedge")) {
+            return "slice";
+        }
+        else if (measure.contains("scoop")) {
+            return "scoop";
+        }
+        else if (measure.contains("cl") ||measure.contains("cL")  ) {
+            return "cl";
+        }
+        else if (measure.contains("inch")) {
+            return "inch";
+        }
+        else if (measure.contains("glass")) {
+            return "glass";
+        }
+        else if (measure.contains("bottle")) {
+            return "bottle";
+        }
+        else if (measure.contains("gr") || measure.contains("lb")) {
+            return "gr";
+        }
+        else if (measure.contains("gal")) {
+            return "gal";
+        }
+        else if (measure.contains("part")) {
+            return "part";
+        }
+        else if (measure.contains("can")) {
+            return "can";
+        }
+        else if (measure.contains("liter") ||measure.contains("quart") || measure.contains("qt")) {
+            return "liter";
+        }
+        else if (measure.contains("dash") || measure.contains("splash")) {
+            return "dash";
+        }
+        else if (measure.contains("count") || measure.contains("cube") || measure.contains("fifth")) {
+            return "count";
+        }
+        else if (measure.contains("chunk") || measure.contains("piece") || measure.contains("whole")) {
+            return "piece";
+        }
+        else if (measure.contains("stick")) {
+            return "stick";
+        }
+        else if (measure.contains("fill") || measure.contains("with") || measure.contains("taste")
+                || measure.contains("top") || measure.contains("up") || measure.contains("of")
+                || measure.contains("garnish")|| measure.contains("full") || measure.contains("chilled")) {
+            return "fill";
+        }
+        else if (measure.contains("drop")) {
+            return "drop";
+        }
+        else if (measure.matches("\\d+(\\.\\d+)?")) {
+            return "count";
+        }
+
+        return measure;
+    }
+
 }
