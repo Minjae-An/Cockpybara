3b223d428aad2f8136fa8ec77c3482c2f42ed3bb
jisu <comjisu0311@naver.com>
2023-07-17 02:54:44 +0900

    add refinded_drinks.csv, final Analysis csv

--- Cockpybara/src/main/java/Main.java
+++ Cockpybara/src/main/java/Main.java
@@ -6,6 +6,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 
@@ -80,7 +81,7 @@ public class Main {
 
 
         //재료와 단위들 리스트 형식으로 묶은 ingredients_list.csv 파일 생성
-
+/*
         String outCsvFile = "src/main/resources/ingredients_list.csv"; // 저장할 CSV 파일의 경로
         try (CSVWriter writer2 = new CSVWriter(new FileWriter(outCsvFile))) {
             // 헤더 라인 작성
@@ -105,7 +106,7 @@ public class Main {
             e.printStackTrace();
         }
 
-
+ */
         // 정제된 재료와 단위 출력
         System.out.println("Unit List:");
         for (String unit : units) {
@@ -114,10 +115,10 @@ public class Main {
         System.out.println("Total Units: " + units.size());
 
         // 원본 칵테일 레시피 CSV 파일의 단위 통일화하여 새로운 CSV 파일 생성
-        //String originalCsvFile = "src\\main\\resources\\all_drinks.csv"; // 원본 칵테일 레시피 CSV 파일의 경로
-        //String refinedCsvFile = "src\\main\\resources\\refined_drinks.csv"; // 정제된 칵테일 레시피 CSV 파일의 경로
+        String originalCsvFile = "src\\main\\resources\\all_drinks.csv"; // 원본 칵테일 레시피 CSV 파일의 경로
+        String refinedCsvFile = "src\\main\\resources\\refined_drinks.csv"; // 정제된 칵테일 레시피 CSV 파일의 경로
 
-       // refineCocktailCsv(originalCsvFile, refinedCsvFile);  //원본 엑셀파일 단위 정제
+        refineCocktailCsv(originalCsvFile, refinedCsvFile);  //원본 엑셀파일 단위 정제
     }
     private static void printMissingIngredient(String ingredient, int rowNumber) {
         System.out.println("Missing Ingredient: " + ingredient + ", Row: " + rowNumber);
@@ -201,41 +202,140 @@ public class Main {
         return measure;
     }
 
+    //기존에는 통합된 단위만 return했던것과 달리 재료량(단위 변경에 따른 재료값도 변경)+통합된 단위 return
+    private static String normalizeUnit2(String measure) {
+        measure = measure.toLowerCase().trim();
+
+        // 단위 단어들을 배열로 분리
+        String[] words = measure.split("\\s+");
+        String firstWord = words[0];
+
+        if (words.length == 1) {
+            if(words[0].contains("fill")){
+                return "fill";
+            }
+            else{
+                return firstWord + " count"; // 재료량만 있는 경우 "숫자 count"로 처리
+            }
+        }
+
+       if (measure.contains("tblsp")) {
+            return firstWord+" tblsp";
+        }
+        else if (measure.contains("tsp") || measure.contains("spoon")) {
+            return firstWord+" tsp";
+        }
+        else if (measure.contains("cup")) {
+            return firstWord+" cup";
+        }
+        else if (measure.contains("slice") || measure.contains("wedge")) {
+            return firstWord+" slice";
+        }
+        else if (measure.contains("scoop")) {
+            return firstWord+" scoop";
+        }
+        else if (measure.contains("inch")) {
+            return firstWord+" inch";
+        }
+        else if (measure.contains("glass")) {
+            return firstWord+" glass";
+        }
+        else if (measure.contains("bottle")) {
+            return firstWord+" bottle";
+        }
+        else if (measure.contains("gr")) {
+            return firstWord+" gr";
+        }
+        else if (measure.contains("lb")) {
+            return firstWord+" lb";
+        }
+        else if (measure.contains("gal")) {
+            return firstWord+" gal";
+        }
+        else if (measure.contains("part")) {
+            return firstWord+" part";
+        }
+        else if (measure.contains("can")) {
+            return firstWord+" can";
+        }
+        else if (measure.contains("liter") || measure.contains("quart") || measure.contains("qt")) {
+            return firstWord+" liter";
+        }
+        else if (measure.contains("count") || measure.contains("cube") || measure.contains("fifth")) {
+            return firstWord+" count";
+        }
+        else if (measure.contains("chunk") || measure.contains("piece") || measure.contains("whole")) {
+            return firstWord+" piece";
+        }
+        else if (measure.contains("stick")) {
+            return firstWord+" stick";
+        }
+        else if (measure.contains("fill") || measure.contains("with") || measure.contains("taste")
+                || measure.contains("top") || measure.contains("up") || measure.contains("of")
+                || measure.contains("garnish") || measure.contains("full") || measure.contains("chilled")) {
+            return "fill";
+        }
+        else if (measure.contains("oz") || measure.contains("jigger") || measure.contains("shot") || measure.contains("cl") || measure.contains("cL")) {
+            if(measure.contains("oz")|| measure.contains("jigger") || measure.contains("shot"))return firstWord+" oz";
+            else if(measure.contains("cl")|| measure.contains("cL")){  //1cl = 0.3 oz
+               return String.format("%.2f", Double.parseDouble(firstWord)*0.3) +" oz";
+           }
+        }
+        else if (measure.contains("dash") || measure.contains("splash")) {
+            if(measure.contains("dash"))return firstWord+" dash";
+            else if(measure.contains("splash")){  //1splash = 3dash
+                return  Double.toString(Double.parseDouble(firstWord)*3)+" dash";
+            }
+        }
+        else if (measure.contains("ml") || measure.contains("dl") || measure.contains("pint")) {
+            if(measure.contains("ml"))return firstWord+" ml";
+            else if(measure.contains("dl")){  //1dl = 100ml
+                return Double.toString(Double.parseDouble(firstWord)*100)+" ml";
+            }
+            else if(measure.contains("pint")){  //1pint = 500ml
+                return Double.toString(Double.parseDouble(firstWord)*500) +" ml";
+            }
+
+        }
+        else if (measure.contains("drop")) {
+            return firstWord+" drop";
+        }
+
+        return measure;
+    }
+
+
     //데이터 정제된 새로운 엑셀 파일만들기
-    private static void refineCocktailCsv(String originalCsvFile, String refinedCsvFile) throws CsvValidationException {
+    private static void refineCocktailCsv(String originalCsvFile, String refinedCsvFile) throws CsvValidationException, IOException {
         try (CSVReader reader = new CSVReader(new FileReader(originalCsvFile));
              CSVWriter writer = new CSVWriter(new FileWriter(refinedCsvFile))) {
 
             String[] nextLine;
-            int rowNumber = 1;
+            writer.writeNext(reader.readNext()); // 복사: 헤더 라인 작성
 
-            // 헤더 라인 복사
-            if ((nextLine = reader.readNext()) != null) {
-                writer.writeNext(nextLine);
-            }
-
+            int rowNumber = 1; // 현재 행 번호
             while ((nextLine = reader.readNext()) != null) {
-                String[] refinedLine = new String[nextLine.length];
-                //기존 라인 복사해서 붙여넣기
-                System.arraycopy(nextLine, 0, refinedLine, 0, nextLine.length);
-
                 for (int i = 9; i <= 23; i++) {
                     String ingredient = nextLine[i];
+                    String measure = nextLine[i + 16];
+
                     if (ingredient == null || ingredient.trim().isEmpty()) {
-                        continue;
+                        continue; // 재료 칸이 비어있으면 건너뛰기
+                    } else {
+                        ingredient = normalizeIngredient(ingredient); // 재료 통일화
                     }
 
-                    String measure = nextLine[i + 16];
                     if (measure.equals("\n") || measure.trim().isEmpty()) {
-                        //비어있는 칸 출력
-                        //printMissingIngredient(ingredient, rowNumber);
-                        measure = "fill";
+                        measure = "fill"; // 단위가 비어있는 경우 fill로 채우기
+                    } else {
+                        measure = normalizeUnit2(measure); // 단위 통일화
                     }
-                    measure = normalizeIngredientUnit(measure);
-                    refinedLine[i+16] = measure;
+
+                    nextLine[i] = ingredient; // 재료 업데이트
+                    nextLine[i + 16] = measure; // 단위 업데이트
                 }
 
-                writer.writeNext(refinedLine);
+                writer.writeNext(nextLine); // 정제된 데이터 작성
                 rowNumber++;
             }
         } catch (IOException e) {
@@ -243,6 +343,7 @@ public class Main {
         }
     }
 
+    //재료 통일
     private static String normalizeIngredient(String ingredient) {
         ingredient = ingredient.toLowerCase().trim();
 
@@ -270,6 +371,7 @@ public class Main {
         else if (ingredient.contains("lime juice") ){
             return "lime juice";
         }
+
         else if (ingredient.contains("soda")|| ingredient.contains("soda water")){
             return "soda";
         }
@@ -285,6 +387,12 @@ public class Main {
         else if (ingredient.contains("whiskey") || ingredient.contains("whisky")) {
             return "whiskey";
         }
+       else if (ingredient.contains("vermouth") ) {
+           return "vermouth";
+       }
+       else if (ingredient.contains("anisette")) {
+           return "anisette";
+       }
         else if (ingredient.contains("anise") || ingredient.contains("anis")) {
             return "anis";
         }
@@ -303,6 +411,12 @@ public class Main {
         else if (ingredient.contains("egg")) {
             return "egg";
         }
+       else if (ingredient.contains("grain alcohol")) {
+           return "everclear";
+       }
+       else if (ingredient.contains("melon liqueur")) {
+           return "melon liqueur";
+       }
         else if (ingredient.contains("demerara sugar") || ingredient.contains("powdered sugar")) {
             return "sugar";
         }
@@ -360,46 +474,12 @@ public class Main {
         else if (ingredient.contains("gin")) {
             return "gin";
         }
-
-
-        return ingredient;
-    }
-    private static String normalizeIngredientUnit(String ingredient) {
-        if (ingredient.equals("\n") || ingredient.trim().isEmpty() || ingredient.contains("fill")) {
-            return "fill";
-        }
-        String[] parts = ingredient.trim().split("\\s+", 2); // Split into quantity and unit
-
-        if (parts.length == 2) {
-            String quantity = parts[0].trim();
-            String unit = parts[1].trim();
-
-            // Handle unit-only case
-
-
-            //양은 소수 둘째자리까지만
-            double quantityValue = Double.parseDouble(quantity);
-
-            try {
-                quantityValue = Double.parseDouble(quantity);
-                quantity = String.format("%.2f", quantityValue);
-            } catch (NumberFormatException e) {
-                // Handle non-numeric quantity, e.g. "fill"
-                quantity = "0";
-                unit = normalizeUnit(unit);
-                return quantity + " " + unit;
-            }
-            // Normalize unit
-            unit = normalizeUnit(unit);
-
-            return quantity + " " + unit;
-        }
+       else if (ingredient.contains("cherries") ) {
+           return "cherry";
+       }
 
         return ingredient;
     }
-
-
-
 }
 
 /*
