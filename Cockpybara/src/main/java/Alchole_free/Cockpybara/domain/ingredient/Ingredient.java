0fcf1bdbaf8ca35a31f07193199dd7b9602620ca
jisu <comjisu0311@naver.com>
2023-07-19 17:14:50 +0900

    feautr#30 branch add Ingredient entity, RecipeIngredient entity

---
+++ Cockpybara/src/main/java/Alchole_free/Cockpybara/domain/ingredient/Ingredient.java
@@ -0,0 +1,24 @@
+﻿package Alchole_free.Cockpybara.domain.ingredient;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Ingredient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Ingredient(String name) {
+        this.name = name;
+    }
+}
---
+++ Cockpybara/src/main/java/Alchole_free/Cockpybara/domain/ingredient/RecipeIngredient.java
@@ -0,0 +1,33 @@
+﻿package Alchole_free.Cockpybara.domain.ingredient;
+import Alchole_free.Cockpybara.domain.cocktail_recipe.CocktailRecipe;
+import javax.persistence.*;
+
+@Entity
+public class RecipeIngredient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cocktail_recipe_id")
+    private CocktailRecipe cocktailRecipe;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ingredient_id")
+    private Ingredient ingredient;
+    private String unit;
+    private String quantity;
+    public RecipeIngredient() {
+        // 기본 생성자
+    }
+    public RecipeIngredient(CocktailRecipe cocktailRecipe, Ingredient ingredient, String unit, String quantity) {
+        this.cocktailRecipe = cocktailRecipe;
+        this.ingredient = ingredient;
+        this.unit = unit;
+        this.quantity = quantity;
+    }
+
+    // Getter와 Setter 메소드 생략
+
+    // 필요에 따라 다른 메소드 추가 가능
+}
