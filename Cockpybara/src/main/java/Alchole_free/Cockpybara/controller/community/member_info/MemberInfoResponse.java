c015adb54e2847e45958112e03416dbbe732e9a8
minjae_an <mj3242@naver.com>
2023-07-21 21:22:18 +0900

    feat : MemberInfoResponse 클래스 추가(#39)
    커뮤니티 뷰에서 유저 PK, 닉네임을 받는데 쓰이는 DTO 클래스 정의

---
+++ Cockpybara/src/main/java/Alchole_free/Cockpybara/controller/community/member_info/MemberInfoResponse.java
@@ -0,0 +1,11 @@
+package Alchole_free.Cockpybara.controller.community.member_info;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class MemberInfoResponse {
+    private final Long userId;
+    private final String alias;
+}
