3fc2fad0dabc8debbe2d44f51dccfbd2e576340b
jisu <comjisu0311@naver.com>
2023-07-22 13:37:32 +0900

    feautr#30 branch add ingredientCategory field in Ingredient entity

--- Cockpybara/src/main/java/Alchole_free/Cockpybara/domain/ingredient/CSVToIngredientObjectMapping.java
+++ Cockpybara/src/main/java/Alchole_free/Cockpybara/domain/ingredient/CSVToIngredientObjectMapping.java
@@ -1,17 +1,21 @@
 package Alchole_free.Cockpybara.domain.ingredient;
 
+//import Alchole_free.Cockpybara.repository.IngredientRepository;
 import com.opencsv.CSVReader;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CSVToIngredientObjectMapping {
     public static void main(String[] args) {
-        String csvFile = "C:\\CockpyPj\\Cockpybara\\Cockpybara\\src\\main\\resources\\ingredients_list.csv"; // 읽을 CSV 파일 경로
 
+        String csvFile = "C:\\CockpyPj\\Cockpybara\\Cockpybara\\src\\main\\resources\\ingredients_list.csv";
         try (CSVReader reader = new CSVReader(new FileReader(csvFile))) {
             CsvToBean<Ingredient> csvToBean = new CsvToBeanBuilder<Ingredient>(reader)
                     .withType(Ingredient.class) // 매핑할 클래스 지정
@@ -20,11 +24,28 @@ public class CSVToIngredientObjectMapping {
 
             List<Ingredient> ingredients = csvToBean.parse(); // CSV 데이터를 객체로 매핑
 
+            // 재료당 단위를 담을 Map 생성
+            Map<String, List<String>> ingredientUnitsMap = new HashMap<>();
+
             for (Ingredient ingredient : ingredients) {
-                System.out.println(ingredient.getId() + ", " + ingredient.getName());
+                String ingredientName = ingredient.getName();
+                // 재료명이 처음 나오는 경우, 새로운 List를 생성하여 단위 추가
+                List<String> units = new ArrayList<>();
+                ingredientUnitsMap.put(ingredientName, units);
+            }
+
+            // 재료와 단위 출력
+            for (Map.Entry<String, List<String>> entry : ingredientUnitsMap.entrySet()) {
+                String ingredientName = entry.getKey();
+                List<String> units = entry.getValue();
+
+                for (String unit : units) {
+                    System.out.println(ingredientName + ", " + unit);
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 }
--- Cockpybara/src/main/java/Alchole_free/Cockpybara/domain/ingredient/Ingredient.java
+++ Cockpybara/src/main/java/Alchole_free/Cockpybara/domain/ingredient/Ingredient.java
@@ -1,6 +1,5 @@
 package Alchole_free.Cockpybara.domain.ingredient;
 
-import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -10,15 +9,37 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor
 @Getter
 public class Ingredient {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
     private String name;
-
+    private String ingredientCategory;
     public Ingredient(String name) {
         this.name = name;
+    }
+
+    // Getter and Setter methods
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {this.name = name;}
+    public String getIngredientCategory() {
+        return ingredientCategory;
+    }
+
+    public void setIngredientCategory(String ingredientCategory) {
+        this.ingredientCategory = ingredientCategory;
     }
 }
