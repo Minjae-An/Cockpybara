ac5a74c0b46382efc643db458784ed7ad8dae3a7
jisu <comjisu0311@naver.com>
2023-07-23 23:03:21 +0900

    Delete IngredientsRepository.java

--- Cockpybara/src/main/java/Alchole_free/Cockpybara/repository/IngredientsRepository.java
+++
@@ -1,9 +0,0 @@
-package Alchole_free.Cockpybra.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import Alchole_free.Cockpybra.model.Ingredient;
-@Repository
-public interface IngredientsRepository extends JpaRepository<Ingredient, Long> {
-    //추가 쿼리 메소드 밑에 작성
-}
