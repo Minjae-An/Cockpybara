48ef894cc8fbc16dec09888c2efa3bee38ce2e63
jisu <comjisu0311@naver.com>
2023-07-23 18:42:20 +0900

    feautr#43 branch modify Ingredient class

--- Cockpybara/src/main/java/Alchole_free/Cockpybara/domain/ingredient/Ingredient.java
+++ Cockpybara/src/main/java/Alchole_free/Cockpybara/domain/ingredient/Ingredient.java
@@ -3,10 +3,7 @@ package Alchole_free.Cockpybara.domain.ingredient;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @NoArgsConstructor
@@ -16,30 +13,12 @@ public class Ingredient {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
-    private String ingredientCategory;
-    public Ingredient(String name) {
+
+    @Enumerated(EnumType.STRING)
+    private IngredientCategory ingredientCategory;
+
+    public Ingredient(String name, IngredientCategory ingredientCategory){
         this.name = name;
-    }
-
-    // Getter and Setter methods
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {this.name = name;}
-    public String getIngredientCategory() {
-        return ingredientCategory;
-    }
-
-    public void setIngredientCategory(String ingredientCategory) {
         this.ingredientCategory = ingredientCategory;
     }
 }
