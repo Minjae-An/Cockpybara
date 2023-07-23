f3cebeaa2ce4a8577ae5ef220121921735adeca8
minjae_an <mj3242@naver.com>
2023-07-23 20:19:53 +0900

    feat : RecipeTaste @NoArgsConstructor 추가(#36)
    JPA 엔티티의 지연 로딩 활용 가능성을 염두하여 protected 접근의
    기본 생성자 추가

--- Cockpybara/src/main/java/Alchole_free/Cockpybara/domain/cocktail_recipe/taste/RecipeTaste.java
+++ Cockpybara/src/main/java/Alchole_free/Cockpybara/domain/cocktail_recipe/taste/RecipeTaste.java
@@ -1,10 +1,13 @@
 package Alchole_free.Cockpybara.domain.cocktail_recipe.taste;
 
 import Alchole_free.Cockpybara.domain.cocktail_recipe.CocktailRecipe;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class RecipeTaste {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
