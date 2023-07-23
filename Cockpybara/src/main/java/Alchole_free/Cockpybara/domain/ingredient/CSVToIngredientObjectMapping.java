831d99fe9eaad602c5d1832404d8184bdcf6bc9c
jisu <comjisu0311@naver.com>
2023-07-23 22:18:34 +0900

    feature#30 branch modify Ingredient class, check ObjectMapping(Issue #30)

--- Cockpybara/src/main/java/Alchole_free/Cockpybara/domain/ingredient/CSVToIngredientObjectMapping.java
+++ Cockpybara/src/main/java/Alchole_free/Cockpybara/domain/ingredient/CSVToIngredientObjectMapping.java
@@ -15,7 +15,7 @@ import java.util.Map;
 public class CSVToIngredientObjectMapping {
     public static void main(String[] args) {
 
-        String csvFile = "C:\\CockpyPj\\Cockpybara\\Cockpybara\\src\\main\\resources\\ingredients_list.csv";
+        String csvFile = "C:\\CockpyPj\\Cockpybara\\Cockpybara\\src\\main\\resources\\ingredients_classification.csv";
         try (CSVReader reader = new CSVReader(new FileReader(csvFile))) {
             CsvToBean<Ingredient> csvToBean = new CsvToBeanBuilder<Ingredient>(reader)
                     .withType(Ingredient.class) // 매핑할 클래스 지정
@@ -24,25 +24,12 @@ public class CSVToIngredientObjectMapping {
 
             List<Ingredient> ingredients = csvToBean.parse(); // CSV 데이터를 객체로 매핑
 
-            // 재료당 단위를 담을 Map 생성
-            Map<String, List<String>> ingredientUnitsMap = new HashMap<>();
-
             for (Ingredient ingredient : ingredients) {
-                String ingredientName = ingredient.getName();
-                // 재료명이 처음 나오는 경우, 새로운 List를 생성하여 단위 추가
-                List<String> units = new ArrayList<>();
-                ingredientUnitsMap.put(ingredientName, units);
+                System.out.println("Ingredient Name: " + ingredient.getName());
+                System.out.println("Ingredient Category: " + ingredient.getCategory());
+                System.out.println("===================================");
             }
 
-            // 재료와 단위 출력
-            for (Map.Entry<String, List<String>> entry : ingredientUnitsMap.entrySet()) {
-                String ingredientName = entry.getKey();
-                List<String> units = entry.getValue();
-
-                for (String unit : units) {
-                    System.out.println(ingredientName + ", " + unit);
-                }
-            }
         } catch (IOException e) {
             e.printStackTrace();
         }
