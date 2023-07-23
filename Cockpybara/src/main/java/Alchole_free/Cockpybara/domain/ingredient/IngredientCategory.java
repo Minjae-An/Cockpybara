9bf77b248fb78014a912a54fb8aaa4fd7ca2b790
jisu <comjisu0311@naver.com>
2023-07-23 18:28:05 +0900

    feautr#43 branch add IngredientCategory enum class

--- Cockpybara/src/main/java/Alchole_free/Cockpybara/domain/ingredient/IngredientCategory.java
+++ Cockpybara/src/main/java/Alchole_free/Cockpybara/domain/ingredient/IngredientCategory.java
@@ -1,5 +1,28 @@
 ﻿package Alchole_free.Cockpybara.domain.ingredient;
 
-public class IngredientCategory {
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
+public enum IngredientCategory {
+    LIQUID("liquid"),
+    FRUIT("fruit"),
+    SYRUP("syrup"),
+    ETC("etc");
+
+    private final String name;
+
+    IngredientCategory(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public static Map<String, IngredientCategory> getNameAndValues() {
+        IngredientCategory[] values = IngredientCategory.values();
+        Map<String, IngredientCategory>nameAndValues = new HashMap<>();
+        Arrays.stream(values).forEach(v -> nameAndValues.put(v.getName(), v));
+        return nameAndValues;
+    }
 }
