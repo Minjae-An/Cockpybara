00f29e52aa8552f90aa89797b16f946057a6c0a4
soz1p <137259205+soz1p@users.noreply.github.com>
2023-07-28 03:28:14 +0900

    Revert "refactor : 아이디찾기 페이지 css구현 및 폰트 적용#63"

--- Cockpybara/src/main/frontend/src/index.js
+++ Cockpybara/src/main/frontend/src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import "./static/fonts/font.css";
 import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
 import App from './App';
-import './index.css'; // 전역 스타일을 import
-//import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <BrowserRouter> {/* Wrap your App component with BrowserRouter */}
@@ -12,5 +9,3 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById('root')
 );
-
-//reportWebVitals();
