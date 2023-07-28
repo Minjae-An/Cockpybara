cb11fa3b2ecc69af6d4897bde6b248fde3e2a47c
minjae_an <mj3242@naver.com>
2023-07-28 21:47:22 +0900

    feat : CocktailRecipeService removeMyRecipe 메서드 추가(#61)
    사용자 레시피 삭제 메서드 구현. 파라미터로 memberId와 cocktailRecipeId를
    받아 해당 되는 엔티티를 조회한 후, 레시피는 쿼리의 where절의 파라미터를
    줄이기 위해 deleteById로 삭제한다. 이후, member는 removeMyRecipe 메서드를
    통해 레시피를 삭제하도록 구성하였다.

--- Cockpybara/src/main/java/Alchole_free/Cockpybara/service/cocktail_recipe/CocktailRecipeService.java
+++ Cockpybara/src/main/java/Alchole_free/Cockpybara/service/cocktail_recipe/CocktailRecipeService.java
@@ -22,19 +22,28 @@ public class CocktailRecipeService {
         return cocktailRecipeRepository.findCocktailRecipeByNameContaining(name);
     }
 
-    public CocktailRecipe findById(Long id){
+    public CocktailRecipe findById(Long id) {
         return cocktailRecipeRepository
                 .findById(id)
                 .orElseThrow(() -> new IllegalStateException("해당 레시피가 존재하지 않습니다."));
     }
 
-    public MyRecipe saveMyRecipe(Long memberId, CocktailRecipe cocktailRecipe){
+    public MyRecipe saveMyRecipe(Long memberId, CocktailRecipe cocktailRecipe) {
         CocktailRecipe savedRecipe = cocktailRecipeRepository.save(cocktailRecipe);
         Member findMember = memberRepository.findById(memberId)
-                .orElseThrow(() -> new IllegalStateException("해당 멤버가 존재하지 않습니다"));
+                .orElseThrow(() -> new IllegalStateException("해당 멤버가 존재하지 않습니다."));
 
         MyRecipe myRecipe = new MyRecipe(findMember, savedRecipe);
         findMember.addNewMyRecipe(myRecipe);
         return myRecipe;
+    }
+
+    public void removeMyRecipe(Long memberId, Long cocktailRecipeId) {
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new IllegalStateException("해당 멤버가 존재하지 않습니다."));
+        CocktailRecipe cocktailRecipe = findById(cocktailRecipeId);
+
+        cocktailRecipeRepository.deleteById(cocktailRecipeId);
+        member.removeMyRecipe(cocktailRecipe);
     }
 }
