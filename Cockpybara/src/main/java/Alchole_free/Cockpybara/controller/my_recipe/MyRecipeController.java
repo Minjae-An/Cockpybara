cba3e2cba6d5ad832e398f88c6907753458e65fa
minjae_an <mj3242@naver.com>
2023-07-28 21:22:10 +0900

    feat : MyRecipeController getRecipeOptionValues 메서드 추가(#61)
    새로운 칵테일 레시피 생성에 필요한 enum 값들을 전달하는
    메서드 정의

--- Cockpybara/src/main/java/Alchole_free/Cockpybara/controller/my_recipe/MyRecipeController.java
+++ Cockpybara/src/main/java/Alchole_free/Cockpybara/controller/my_recipe/MyRecipeController.java
@@ -1,7 +1,13 @@
 package Alchole_free.Cockpybara.controller.my_recipe;
 
 import Alchole_free.Cockpybara.controller.my_recipe.add_new_my_recipe.AddNewMyRecipeRequest;
+import Alchole_free.Cockpybara.controller.my_recipe.recipe_options.RecipeOptionsResponse;
+import Alchole_free.Cockpybara.domain.cocktail_recipe.AlcoholicType;
+import Alchole_free.Cockpybara.domain.cocktail_recipe.Category;
 import Alchole_free.Cockpybara.domain.cocktail_recipe.CocktailRecipe;
+import Alchole_free.Cockpybara.domain.cocktail_recipe.Glass;
+import Alchole_free.Cockpybara.domain.cocktail_recipe.taste.Taste;
+import Alchole_free.Cockpybara.domain.ingredient.Unit;
 import Alchole_free.Cockpybara.service.cocktail_recipe.CocktailRecipeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +20,20 @@ import java.time.LocalDateTime;
 @RequestMapping("/user/{userId}/my-recipe")
 public class MyRecipeController {
     private final CocktailRecipeService cocktailRecipeService;
+
+    @GetMapping
+    public ResponseEntity<RecipeOptionsResponse> getRecipeOptionValues() {
+        RecipeOptionsResponse recipeOptionsResponse = new RecipeOptionsResponse(
+                Glass.values(),
+                Category.values(),
+                AlcoholicType.values(),
+                Taste.values(),
+                Unit.values()
+        );
+
+        return ResponseEntity.ok(recipeOptionsResponse);
+    }
+
 
     @PostMapping
     public ResponseEntity<String> addNewMyRecipe(@PathVariable Long userId,
