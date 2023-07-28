8bfd6214904f3132d453ff6c356cfa8bb1fcf134
minjae_an <mj3242@naver.com>
2023-07-28 20:41:01 +0900

    feat : AddNewMyRecipeRequest 추가(#61)
    새로운 나만의 레시피 생성 요청을 위해 사용되는 DTO 클래스 정의.
    기존의 CocktailRecipe 필드들의 검증 조건들을 바탕으로 필드들 검증.
    
    편의를 위하여 CocktailRecipe로의 변환 메서드 to를 추가로 정의하였다.

---
+++ Cockpybara/src/main/java/Alchole_free/Cockpybara/controller/my_recipe/add_new_my_recipe/AddNewMyRecipeRequest.java
@@ -0,0 +1,54 @@
+package Alchole_free.Cockpybara.controller.my_recipe.add_new_my_recipe;
+
+import Alchole_free.Cockpybara.domain.cocktail_recipe.AlcoholicType;
+import Alchole_free.Cockpybara.domain.cocktail_recipe.Category;
+import Alchole_free.Cockpybara.domain.cocktail_recipe.CocktailRecipe;
+import Alchole_free.Cockpybara.domain.cocktail_recipe.Glass;
+import Alchole_free.Cockpybara.domain.cocktail_recipe.taste.RecipeTaste;
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class AddNewMyRecipeRequest {
+    @NotNull
+    @Length(max = 255)
+    private String name;
+    @NotNull
+    private AlcoholicType alcoholicType;
+    @NotNull
+    private Category category;
+    @NotNull
+    private String drinkImgPath;
+    @NotNull
+    private Glass glass;
+    @NotNull
+    @Length(min = 20, max = 300)
+    private String instruction;
+    private Boolean isMemberRecipe;
+    private LocalDateTime createdAt;
+
+    @NotNull
+    @Size(max = 3)
+    private List<RecipeTaste> tastes = new ArrayList<>();
+
+    public CocktailRecipe to() {
+        CocktailRecipe cocktailRecipe = new CocktailRecipe(
+                name,
+                alcoholicType,
+                category,
+                drinkImgPath,
+                glass,
+                instruction,
+                isMemberRecipe,
+                createdAt
+        );
+        cocktailRecipe.setTastes(tastes);
+        return cocktailRecipe;
+    }
+}
