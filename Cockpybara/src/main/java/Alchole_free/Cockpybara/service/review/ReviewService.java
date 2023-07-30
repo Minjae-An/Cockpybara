3f3405ff35cb06b146124e319f251a5b3acba9c7
minjae_an <mj3242@naver.com>
2023-07-30 17:32:24 +0900

    feat : ReviewService 클래스 추가(#76)
    수행하는 작업과 클래스의 명확한 매칭을 위해 Review 관련 비즈니스
    로직을 전담하는 서비스 클래스 정의. addReview 메서드를 통해
    리뷰 추가 연산 수행

---
+++ Cockpybara/src/main/java/Alchole_free/Cockpybara/service/review/ReviewService.java
@@ -0,0 +1,30 @@
+package Alchole_free.Cockpybara.service.review;
+
+import Alchole_free.Cockpybara.domain.cocktail_recipe.CocktailRecipe;
+import Alchole_free.Cockpybara.domain.cocktail_recipe.review.Review;
+import Alchole_free.Cockpybara.domain.member.Member;
+import Alchole_free.Cockpybara.repository.CocktailRecipeRepository;
+import Alchole_free.Cockpybara.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ReviewService {
+    private final CocktailRecipeRepository cocktailRecipeRepository;
+    private final MemberRepository memberRepository;
+
+    public void addReview(Long recipeId, Long memberId, Integer stars, String review){
+        CocktailRecipe cocktailRecipe = cocktailRecipeRepository.findById(recipeId)
+                .orElseThrow(() -> new IllegalStateException("해당 레시피가 존재하지 않습니다."));
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new IllegalStateException("해당 멤버가 존재하지 않습니다."));
+
+        Review newReview = new Review(cocktailRecipe, member, stars, review);
+
+        cocktailRecipe.getReviews().add(newReview);
+    }
+
+}
