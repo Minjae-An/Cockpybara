e13c9e0f1d0106d5ea18ff50272429dd57a77583
jisu <comjisu0311@naver.com>
2023-07-30 21:39:13 +0900

    feat: IngredientUnitMap add(#82)

---
+++ Cockpybara/src/main/java/Alchole_free/Cockpybara/domain/ingredient/IngredientUnitMap.java
@@ -0,0 +1,19 @@
+﻿package Alchole_free.Cockpybara.domain.ingredient;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class IngredientUnitMap {
+    public static Map<IngredientCategory, String[]> createIngredientUnitMap() {
+        Map<IngredientCategory, String[]> ingredientUnitMap = new HashMap<>();
+
+        // Add units for each ingredient category
+        ingredientUnitMap.put(IngredientCategory.LIQUID, new String[]{"ml", "oz"});
+        ingredientUnitMap.put(IngredientCategory.FRUIT, new String[]{"piece", "slice"});
+        ingredientUnitMap.put(IngredientCategory.SYRUP, new String[]{"ml", "tbsp"});
+        ingredientUnitMap.put(IngredientCategory.ETC, new String[]{"g", "tsp"});
+
+        return ingredientUnitMap;
+    }
+}
+
