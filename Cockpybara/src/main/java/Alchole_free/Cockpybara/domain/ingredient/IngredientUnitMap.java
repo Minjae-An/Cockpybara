155f8629ac47ba3368628cf684512056dfdc9b6e
jisu <comjisu0311@naver.com>
2023-08-01 01:40:26 +0900

    refactor: IngredientUnitMap 수정(#82) 카테고리별 단위 다시 정리후 수정 단위 오류 있는 csv파일 부분 데이터 수정

--- Cockpybara/src/main/java/Alchole_free/Cockpybara/domain/ingredient/IngredientUnitMap.java
+++ Cockpybara/src/main/java/Alchole_free/Cockpybara/domain/ingredient/IngredientUnitMap.java
@@ -7,7 +7,7 @@ public class IngredientUnitMap {
     public static Map<IngredientCategory, String[]> createIngredientUnitMap() {
         Map<IngredientCategory, String[]> ingredientUnitMap = new HashMap<>();
 
-        //각 카테고리별 해당 단위 맵 리턴
+        //각 카테고리별 해당 단위 맵 리턴 - 단위 오류시 추후 수정
         ingredientUnitMap.put(IngredientCategory.LIQUID, new String[]{"fill", "oz", "part", "liter", "count", "ml", "dash", "drop", "tsp", "cup", "glass", "bottle", "slice", "tblsp", "can", "gal"});
         ingredientUnitMap.put(IngredientCategory.FRUIT, new String[]{"count", "cup", "piece", "fill", "gr", "slice", "part", "lb", "tblsp", "oz"});
         ingredientUnitMap.put(IngredientCategory.SYRUP, new String[]{"fill", "tsp", "oz", "drop", "gr", "tblsp", "part", "dash", "count", "cup", "ml"});
