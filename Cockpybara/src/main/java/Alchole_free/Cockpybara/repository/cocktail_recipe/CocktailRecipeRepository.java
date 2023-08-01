1c46f625f7a8443dc9c72973c2aa8c8ea8b09fc1
minjae_an <mj3242@naver.com>
2023-08-01 20:43:47 +0900

    feat : CocktailRepository CocktailRecipeCustom 상속 설정(#10)

--- Cockpybara/src/main/java/Alchole_free/Cockpybara/repository/cocktail_recipe/CocktailRecipeRepository.java
+++ Cockpybara/src/main/java/Alchole_free/Cockpybara/repository/cocktail_recipe/CocktailRecipeRepository.java
@@ -1,4 +1,4 @@
-package Alchole_free.Cockpybara.repository;
+package Alchole_free.Cockpybara.repository.cocktail_recipe;
 
 import Alchole_free.Cockpybara.domain.cocktail_recipe.CocktailRecipe;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface CocktailRecipeRepository extends JpaRepository<CocktailRecipe, Long>{
+public interface CocktailRecipeRepository extends JpaRepository<CocktailRecipe, Long>, CocktailRepositoryCustom{
 
     List<CocktailRecipe> findCocktailRecipeByNameContaining(String name);
 }
