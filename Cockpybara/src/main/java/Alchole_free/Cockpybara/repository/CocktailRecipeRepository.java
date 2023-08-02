7b039cfa55c902e39d3b1696aa17dacf1279f09a
jisu <comjisu0311@naver.com>
2023-08-02 10:47:44 +0900

    refactor CocktailRecipeRepository(#38) 레시피 특정 기간별 내림차순 메서드 작성 레시피 전체 내림차순 메서드 작성

--- Cockpybara/src/main/java/Alchole_free/Cockpybara/repository/CocktailRecipeRepository.java
+++ Cockpybara/src/main/java/Alchole_free/Cockpybara/repository/CocktailRecipeRepository.java
@@ -4,10 +4,13 @@ import Alchole_free.Cockpybara.domain.cocktail_recipe.CocktailRecipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface CocktailRecipeRepository extends JpaRepository<CocktailRecipe, Long>{
 
     List<CocktailRecipe> findCocktailRecipeByNameContaining(String name);
+    List<CocktailRecipe> findByCreatedAtBetweenOrderByCreatedAtDesc(LocalDateTime startDateTime, LocalDateTime endDateTime);
+    List<CocktailRecipe> findAllByOrderByCreatedAtDesc();
 }
