527ff4137792341924372d5e382e9494fe1d7e2c
drimh <107299318+drimh@users.noreply.github.com>
2023-08-03 21:56:03 +0900

    feat: 서브 메뉴바 수정

--- Cockpybara/src/main/frontend/src/DetailHeader.js
+++ Cockpybara/src/main/frontend/src/DetailHeader.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import NewMenu from './NewMenu';
+import NewNavBar from './NewNavBar';
 
 function DetailHeader() {
 
   return (
     <div>
         <NewMenu />
+        <NewNavBar />
     </div>
   );
 };
--- Cockpybara/src/main/frontend/src/NewMenu.js
+++ Cockpybara/src/main/frontend/src/NewMenu.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './NewMenu.css';
 
-const NewMenu = () => { // 여기에 menuClassName 추가
+const NewMenu = () => { 
 
     return (
         <div className="menu-container">
-            <ul className="menuClassName"> {/* 여기에 menuClassName 사용 */}
+            <ul className="menuClassName"> 
                 <li className='new_about'><Link to="/about">About</Link></li>
                 <li className='new_recipe'><Link to="/recipe">Recipe</Link></li>
                 <li className='new_community'><Link to="/community/{userId}">Community</Link></li>
