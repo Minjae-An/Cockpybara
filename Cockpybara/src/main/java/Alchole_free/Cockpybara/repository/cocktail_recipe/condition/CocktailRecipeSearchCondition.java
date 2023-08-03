c806948f4a5c6fa506581fbf55497d0e1df440b1
minjae_an <mj3242@naver.com>
2023-08-03 21:10:35 +0900

    refactor : 칵테일 검색 조건 DTO 필드 수정(#10)
    이름을 제외한 나머지 조건들의 경우 중복적으로 적용할 수 있어야
    하기에 List 형태로 필드들을 변경하여 재정의

--- Cockpybara/src/main/java/Alchole_free/Cockpybara/repository/cocktail_recipe/condition/CocktailRecipeSearchCondition.java
+++ Cockpybara/src/main/java/Alchole_free/Cockpybara/repository/cocktail_recipe/condition/CocktailRecipeSearchCondition.java
@@ -4,15 +4,20 @@ import Alchole_free.Cockpybara.domain.cocktail_recipe.AlcoholicType;
 import Alchole_free.Cockpybara.domain.cocktail_recipe.Category;
 import Alchole_free.Cockpybara.domain.cocktail_recipe.Glass;
 import Alchole_free.Cockpybara.domain.cocktail_recipe.taste.RecipeTaste;
+import Alchole_free.Cockpybara.domain.ingredient.IngredientCategory;
+import Alchole_free.Cockpybara.domain.ingredient.RecipeIngredient;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 public class CocktailRecipeSearchCondition {
     private String name;
-    private AlcoholicType alcoholicType;
-    private Category category;
-    private Glass glass;
 
-    private Boolean isMemberRecipe=false;
-    private RecipeTaste recipeTaste;
+    private List<AlcoholicType> alcoholicTypes;
+    private List<Category> categories;
+    private List<Glass> glasses;
+    private List<Boolean> isMemberRecipes;
+    private List<RecipeTaste> recipeTastes;
+    private List<IngredientCategory> ingredientCategories;
 }
