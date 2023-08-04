fe36b8321e0a19a7ad933fc025f720ad84d5afa8
minjae_an <mj3242@naver.com>
2023-08-04 21:27:02 +0900

    refactor : 칵테일 레시피 검색 조건 DTO 필드 타입 수정(#10)
    recipeTastes 필드의 이름을 tastes로, 타입을 Taste로 전환.
    굳이 중계 엔티티 타입으로 검색 조건을 받아올 필요가 없기 떄문.

--- Cockpybara/src/main/java/Alchole_free/Cockpybara/repository/cocktail_recipe/condition/CocktailRecipeSearchCondition.java
+++ Cockpybara/src/main/java/Alchole_free/Cockpybara/repository/cocktail_recipe/condition/CocktailRecipeSearchCondition.java
@@ -3,9 +3,8 @@ package Alchole_free.Cockpybara.repository.cocktail_recipe.condition;
 import Alchole_free.Cockpybara.domain.cocktail_recipe.AlcoholicType;
 import Alchole_free.Cockpybara.domain.cocktail_recipe.Category;
 import Alchole_free.Cockpybara.domain.cocktail_recipe.Glass;
-import Alchole_free.Cockpybara.domain.cocktail_recipe.taste.RecipeTaste;
+import Alchole_free.Cockpybara.domain.cocktail_recipe.taste.Taste;
 import Alchole_free.Cockpybara.domain.ingredient.IngredientCategory;
-import Alchole_free.Cockpybara.domain.ingredient.RecipeIngredient;
 import lombok.Data;
 
 import java.util.List;
@@ -18,6 +17,6 @@ public class CocktailRecipeSearchCondition {
     private List<Category> categories;
     private List<Glass> glasses;
     private List<Boolean> isMemberRecipes;
-    private List<RecipeTaste> recipeTastes;
+    private List<Taste> tastes;
     private List<IngredientCategory> ingredientCategories;
 }
