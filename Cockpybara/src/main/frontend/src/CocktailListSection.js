6175d67a37e7c28c8568b49b32f87fbb9a9bf6b1
drimh <107299318+drimh@users.noreply.github.com>
2023-08-09 13:55:30 +0900

    refactor: 칵테일 리스트 버튼

--- Cockpybara/src/main/frontend/src/CocktailListSection.js
+++ Cockpybara/src/main/frontend/src/CocktailListSection.js
@@ -1,8 +1,11 @@
 // CocktailListSection.js
 import React, { useState, useEffect } from 'react';
+import './CocktailListSection.css';
 
 const CocktailListSection = () => {
   const [cocktailList, setCocktailList] = useState([]);
+  const [sortBy, setSortBy] = useState('recommended');
+  const [selectedButton, setSelectedButton] = useState('recommended');
 
   useEffect(() => {
     // 더미 데이터를 사용하여 프론트에서 확인할 수 있도록 함
@@ -24,23 +27,52 @@ const CocktailListSection = () => {
     );
   };
 
+  const handleSortByClick = (sortType) => {
+    setSortBy(sortType);
+    setSelectedButton(sortType);
+  };
+
   return (
-    <div style={{ backgroundColor: 'lightgreen' }}>
-      <h2>칵테일 나열</h2>
-      <ul>
-        {cocktailList.map((cocktail) => (
-          <li key={cocktail.id}>
-            <h3>{cocktail.name}</h3>
-            <p>{cocktail.description}</p>
-            <p>Taste: {cocktail.taste}</p>
-            <p>Recommended: {cocktail.recommended ? 'Yes' : 'No'}</p>
-            {/* 추천 유무에 따른 버튼 표시 */}
-            <button onClick={() => toggleRecommendation(cocktail.id)}>
-              {cocktail.recommended ? '추천 취소' : '추천하기'}
-            </button>
-          </li>
-        ))}
-      </ul>
+    <div className="cockList-box">
+      <div className="selectList-box">
+        <div className="selectList-contents"> 
+        <button
+          className={selectedButton === 'popular' ? 'selected' : ''}
+          onClick={() => handleSortByClick('popular')}
+        >
+          인기순
+        </button>
+          <button
+            className={selectedButton === 'latest' ? 'selected' : ''}
+            onClick={() => handleSortByClick('latest')}
+          >
+            최신순
+          </button>
+          <button
+            className={selectedButton === 'alphabetical' ? 'selected' : ''}
+            onClick={() => handleSortByClick('alphabetical')}
+          >
+            가나다순
+          </button>
+        </div>
+      </div>
+      <div className="cockList-contents">
+        <h2>칵테일 나열</h2>
+        <ul>
+          {cocktailList.map((cocktail) => (
+            <li key={cocktail.id}>
+              <h3>{cocktail.name}</h3>
+              <p>{cocktail.description}</p>
+              <p>Taste: {cocktail.taste}</p>
+              <p>Recommended: {cocktail.recommended ? 'Yes' : 'No'}</p>
+              {/* 추천 유무에 따른 버튼 표시 */}
+              <button onClick={() => toggleRecommendation(cocktail.id)}>
+                {cocktail.recommended ? '추천 취소' : '추천하기'}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
