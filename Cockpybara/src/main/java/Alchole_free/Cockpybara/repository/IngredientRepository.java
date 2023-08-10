4c03ccbee8f6090549da5dfe7996e5b3440e7879
minjae_an <mj3242@naver.com>
2023-08-10 16:16:51 +0900

    feat : IngredientRepository findByName 추가(#114)
    DB 더미 데이터셋 insert를 위해 추가

--- Cockpybara/src/main/java/Alchole_free/Cockpybara/repository/IngredientRepository.java
+++ Cockpybara/src/main/java/Alchole_free/Cockpybara/repository/IngredientRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
     public List<Ingredient> findIngredientByNameContaining(String name);
+    public Optional<Ingredient> findByName(String name);
 }
 
