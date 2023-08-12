6ec3d2152dc9cd6257e8033e36354a4e7a2943fc
minjae_an <mj3242@naver.com>
2023-08-12 18:34:07 +0900

    feat : ReviewTaste protected 기본 생성자 추가(#127)

--- Cockpybara/src/main/java/Alchole_free/Cockpybara/domain/cocktail_recipe/review/ReviewTaste.java
+++ Cockpybara/src/main/java/Alchole_free/Cockpybara/domain/cocktail_recipe/review/ReviewTaste.java
@@ -1,11 +1,14 @@
 package Alchole_free.Cockpybara.domain.cocktail_recipe.review;
 
 import Alchole_free.Cockpybara.domain.cocktail_recipe.taste.Taste;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class ReviewTaste {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,4 +19,9 @@ public class ReviewTaste {
 
     @Enumerated(value = EnumType.STRING)
     private Taste taste;
+
+    public ReviewTaste(Review review, Taste taste) {
+        this.review = review;
+        this.taste = taste;
+    }
 }
