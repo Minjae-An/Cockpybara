543d653f94c27a31618f778cd8be92be6382581e
minjae_an <mj3242@naver.com>
2023-08-13 17:59:12 +0900

    feat : LikesController getLikes 메서드 추가(#124)
    즐겨찾기 리스트 조회 메서드 추가.
    ResponseEntity와 LikeDTO 리스트를 통해 응답한다.

--- Cockpybara/src/main/java/Alchole_free/Cockpybara/controller/likes/LikesController.java
+++ Cockpybara/src/main/java/Alchole_free/Cockpybara/controller/likes/LikesController.java
@@ -1,6 +1,7 @@
 package Alchole_free.Cockpybara.controller.likes;
 
 import Alchole_free.Cockpybara.controller.likes.add_like.AddLikeResponse;
+import Alchole_free.Cockpybara.controller.likes.like_list.LikeDTO;
 import Alchole_free.Cockpybara.domain.cocktail_recipe.CocktailRecipe;
 import Alchole_free.Cockpybara.domain.member.Member;
 import Alchole_free.Cockpybara.domain.member.likes.Like;
@@ -10,22 +11,30 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/user/{userId}/likes")
+@RequestMapping("/user/{userId}")
 public class LikesController {
     private final MemberService memberService;
 
-    @PostMapping("/{recipeId}")
+    @PostMapping("/likes/{recipeId}")
     public ResponseEntity<AddLikeResponse> addLike(@PathVariable Long userId, @PathVariable Long recipeId) {
         AddLikeResponse addLikeResponse = memberService.addLike(userId, recipeId);
         return ResponseEntity.ok(addLikeResponse);
     }
 
-    @DeleteMapping("/{recipeId}")
+    @DeleteMapping("/likes/{recipeId}")
     public ResponseEntity<String> removeLike(@PathVariable Long userId, @PathVariable Long recipeId) {
         memberService.removeLike(userId, recipeId);
 
         return ResponseEntity.ok("successfully remove like");
+    }
+
+    @GetMapping("/likes-list")
+    public ResponseEntity<List<LikeDTO>> getLikes(@PathVariable Long userId) {
+        List<LikeDTO> likes = memberService.getLikes(userId);
+        return ResponseEntity.ok(likes);
     }
 }
