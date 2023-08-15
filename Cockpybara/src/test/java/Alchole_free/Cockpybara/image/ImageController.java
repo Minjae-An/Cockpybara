41ed70b5091856326a8feea59e7046fec7ba7f54
jisu <comjisu0311@naver.com>
2023-08-15 17:08:28 +0900

    refactor ImageController, imageService(#131) 테스트용 controller, service 코드 구현

---
+++ Cockpybara/src/test/java/Alchole_free/Cockpybara/image/ImageController.java
@@ -0,0 +1,28 @@
+﻿package Alchole_free.Cockpybara.image;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api")
+public class ImageController {
+
+    private final ImageService imageService;
+
+    @Autowired
+    public ImageController(ImageService imageService) {
+        this.imageService = imageService;
+    }
+
+    @PostMapping("/image-upload")
+    public ResponseEntity<String> uploadImage(@RequestPart MultipartFile file) {
+        //이미지 업로드하고 url받기
+        String imageUrl = imageService.uploadImage(file);
+        return ResponseEntity.ok(imageUrl);
+    }
+}
---
+++ Cockpybara/src/test/java/Alchole_free/Cockpybara/image/ImageService.java
@@ -0,0 +1,24 @@
+﻿package Alchole_free.Cockpybara.image;
+
+import com.google.api.services.drive.Drive;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class ImageService {
+
+    private final Drive driveService; // Google Drive 연동을 위한 서비스
+
+    @Autowired
+    public ImageService(Drive driveService) {
+        this.driveService = driveService;
+    }
+
+    public String uploadImage(MultipartFile file) {
+        // 이미지 업로드 로직 구현
+        // 구글 드라이브에 파일 업로드하고 URL을 반환
+        String test = "testurl";
+        return test;
+    }
+}
