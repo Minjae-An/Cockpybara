3daa64a8c149965f815d7312db283002f9460663
minjae_an <mj3242@naver.com>
2023-08-19 21:58:03 +0900

    feat : ErrorResponse 추가(#137)
    예외 처리시 단순한 응답 처리를 위한 응답 객체 정의.
    필드로 예외 메시지를 가진다.

---
+++ Cockpybara/src/main/java/Alchole_free/Cockpybara/exception/ErrorResponse.java
@@ -0,0 +1,10 @@
+package Alchole_free.Cockpybara.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ErrorResponse {
+    private String message;
+}
