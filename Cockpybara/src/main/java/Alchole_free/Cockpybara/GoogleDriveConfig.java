a5fb7d9bc50b3db0bd3a4090cf11a66c4bafb41a
jisu <comjisu0311@naver.com>
2023-08-24 20:37:21 +0900

    refactor ImageService,GoogleDriveConfig(#131) 수정 및 테스트 확인

--- Cockpybara/src/main/java/Alchole_free/Cockpybara/GoogleDriveConfig.java
+++ Cockpybara/src/main/java/Alchole_free/Cockpybara/GoogleDriveConfig.java
@@ -1,7 +1,7 @@
 package Alchole_free.Cockpybara;
-
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.auth.oauth2.*;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.drive.Drive;
@@ -12,22 +12,55 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
+import java.util.Arrays;
 
 @Configuration
 public class GoogleDriveConfig {
+
+    private final String CLIENT_ID = "789626258147-7lrhn10mtttcjlvtru2r1l9dn5lckib6.apps.googleusercontent.com";
+    private final String CLIENT_SECRET = "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";
+    private final String REFRESH_TOKEN = "1//04J5xcTyKCJldCgYIARAAGAQSNwF-L9IrBH5h3M1RE0oZ4HerbpISYp68hWvSL0SF-DERBluDat1clUKmlhXq-5LxRO7SM_ze5Y8";
+    private final String APPLICATION_NAME = "Cockpybara";
 
     @Bean
     public Drive driveService() throws IOException, GeneralSecurityException {
         // Google Drive 인증 및 설정
         JsonFactory jsonFactory = new JacksonFactory();
+        HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
 
-        // client_secrets.json 파일을 읽어와서 인증 정보 설정
-        InputStream credentialsStream = getClass().getResourceAsStream("/client_secrets.json");
-        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(jsonFactory, new InputStreamReader(credentialsStream));
+        // GoogleCredential 객체 생성
+        GoogleCredential credential = authorize(jsonFactory, httpTransport);
 
-        // DriveScopes 등 다른 설정도 추가 가능
-        return new Drive.Builder(GoogleNetHttpTransport.newTrustedTransport(), jsonFactory, null)
-                .setApplicationName("Cockpybara")
+        // Drive 객체 생성
+        return new Drive.Builder(httpTransport, jsonFactory, credential)
+                .setApplicationName(APPLICATION_NAME)
                 .build();
+    }
+
+    private GoogleCredential authorize(JsonFactory jsonFactory, HttpTransport httpTransport) throws IOException {
+        GoogleClientSecrets clientSecrets = loadClientSecrets(jsonFactory);
+
+        // OAuth 2.0 인증 코드 플로우 설정
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                httpTransport, jsonFactory, clientSecrets,
+                Arrays.asList("https://www.googleapis.com/auth/drive"))
+                .build();
+
+        // GoogleCredential 객체 생성
+        GoogleCredential credential = new GoogleCredential.Builder()
+                .setTransport(httpTransport)
+                .setJsonFactory(jsonFactory)
+                .setClientSecrets(CLIENT_ID, CLIENT_SECRET)
+                .build();
+
+        // 이미 얻어온 리프래시 토큰 설정
+        credential.setRefreshToken(REFRESH_TOKEN);
+
+        return credential;
+    }
+
+    private GoogleClientSecrets loadClientSecrets(JsonFactory jsonFactory) throws IOException {
+        InputStream inputStream = getClass().getResourceAsStream("/client_secrets.json");
+        return GoogleClientSecrets.load(jsonFactory, new InputStreamReader(inputStream));
     }
 }
--- Cockpybara/src/main/java/Alchole_free/Cockpybara/image/ImageService.java
+++ Cockpybara/src/main/java/Alchole_free/Cockpybara/image/ImageService.java
@@ -1,14 +1,20 @@
 package Alchole_free.Cockpybara.image;
 
+import com.google.api.client.http.InputStreamContent;
 import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.model.File;
+import com.google.api.client.http.FileContent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.util.Collections;
+
 @Service
 public class ImageService {
 
-    private final Drive driveService; // Google Drive 연동을 위한 서비스
+    private final Drive driveService;
 
     @Autowired
     public ImageService(Drive driveService) {
@@ -16,9 +22,37 @@ public class ImageService {
     }
 
     public String uploadImage(MultipartFile file) {
-        // 이미지 업로드 로직 구현
-        // 구글 드라이브에 파일 업로드하고 URL을 반환
-        String test = "testurl";
-        return test;
+        try {
+            // 파일 메타데이터 생성
+            File fileMetadata = new File();
+            fileMetadata.setName(file.getOriginalFilename());
+            fileMetadata.setParents(Collections.singletonList("1r6EXdUBsv9ndLwt00ZGUJMfvCdvdccqa")); // 부모 폴더 ID 설정
+
+            // 파일 컨텐츠 생성
+            InputStreamContent mediaContent = new InputStreamContent(file.getContentType(), file.getInputStream());
+
+            // 파일 업로드
+            File uploadedFile = driveService.files().create(fileMetadata, mediaContent)
+                    .setFields("id, webViewLink") // 업로드한 파일의 ID와 웹 링크만 가져오기
+                    .execute();
+
+            // 업로드한 파일의 웹 링크 반환
+            String uploadedImageUrl = uploadedFile.getWebViewLink();
+            return uploadedImageUrl;
+
+        } catch (IOException e) {
+            // 파일 업로드 중 IOException 발생 시 처리
+            e.printStackTrace();
+            return "Error uploading image: " + e.getMessage();
+        } catch (Exception e) {
+            // 기타 예외 발생 시 처리
+            e.printStackTrace();
+            return "Error uploading image: An unexpected error occurred.";
+        }
     }
 }
+
+
+
+
+
