36c9b212f2468edbfd0e0845c31b59962769b99e
jisu <comjisu0311@naver.com>
2023-08-31 20:30:30 +0900

    refactor ImageController(#156) 앤드포인트 변경

--- Cockpybara/src/main/java/Alchole_free/Cockpybara/image/ImageController.java
+++ Cockpybara/src/main/java/Alchole_free/Cockpybara/image/ImageController.java
@@ -1,12 +1,11 @@
 package Alchole_free.Cockpybara.image;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/user/{userid}")
 public class ImageController {
 
     private final ImageService imageService;
@@ -16,7 +15,7 @@ public class ImageController {
         this.imageService = imageService;
     }
 
-    @PostMapping("/image-upload")
+    @PostMapping("/my-page-picture")
     public String uploadImage(@RequestParam("file") MultipartFile file,
                               @RequestParam("email") String email) {
         return imageService.uploadImage(file, email);
