25eed2339094e56b00c41ca83ce04d5cab27f959
minjae_an <mj3242@naver.com>
2023-09-01 20:40:04 +0900

    feat : ReviewRepository findReviewByCocktailRecipe 추가(#134)
    칵테일 레시피에 리뷰를 제공하는 API를 따로 규정함에 따라
    칵테일 레시피를 통해 리뷰를 조회하는 메서드 새로 정의.

--- Cockpybara/src/main/java/Alchole_free/Cockpybara/repository/ReviewRepository.java
+++ Cockpybara/src/main/java/Alchole_free/Cockpybara/repository/ReviewRepository.java
@@ -1,7 +1,10 @@
 package Alchole_free.Cockpybara.repository;
 
+import Alchole_free.Cockpybara.domain.cocktail_recipe.CocktailRecipe;
 import Alchole_free.Cockpybara.domain.cocktail_recipe.review.Review;
 import Alchole_free.Cockpybara.domain.member.Member;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +13,5 @@ import java.util.List;
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     List<Review> findReviewByMember(Member member);
+    Page<Review> findReviewByCocktailRecipe(CocktailRecipe cocktailRecipe, Pageable pageable);
 }
