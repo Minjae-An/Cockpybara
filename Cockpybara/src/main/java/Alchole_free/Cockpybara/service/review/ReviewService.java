d0968d9f4590edea8d1c32db7fd5f27efa03a80b
minjae_an <mj3242@naver.com>
2023-09-01 20:42:07 +0900

    feat : ReviewService getReviews 메서드 추가(#134)
    리뷰를 조회하는 메서드 추가. readOnly 트랜잭션 처리
    recipeId와 pageRequest를 받아 ReviewRepository를 통해
    리뷰 데이터를 페이지 단위로 받아온 후 이를 ReviewDTO 형태로
    매핑하여 CustomPageResponse를 통해 제공하는 형태로 로직 구성.

--- Cockpybara/src/main/java/Alchole_free/Cockpybara/service/review/ReviewService.java
+++ Cockpybara/src/main/java/Alchole_free/Cockpybara/service/review/ReviewService.java
@@ -1,5 +1,6 @@
 package Alchole_free.Cockpybara.service.review;
 
+import Alchole_free.Cockpybara.controller.cocktailrecipe.recipe_detail.review.ReviewDTO;
 import Alchole_free.Cockpybara.controller.review.commented_recipes.CommentedRecipesResponse;
 import Alchole_free.Cockpybara.domain.cocktail_recipe.CocktailRecipe;
 import Alchole_free.Cockpybara.domain.cocktail_recipe.review.Review;
@@ -9,6 +10,7 @@ import Alchole_free.Cockpybara.domain.member.Member;
 import Alchole_free.Cockpybara.repository.cocktail_recipe.CocktailRecipeRepository;
 import Alchole_free.Cockpybara.repository.MemberRepository;
 import Alchole_free.Cockpybara.repository.ReviewRepository;
+import Alchole_free.Cockpybara.util.pagination.CustomPageRequest;
 import Alchole_free.Cockpybara.util.pagination.CustomPageResponse;
 import Alchole_free.Cockpybara.util.pagination.PagingUtil;
 import lombok.RequiredArgsConstructor;
@@ -64,6 +66,26 @@ public class ReviewService {
         Page<CommentedRecipesResponse> pageResult = PagingUtil.listToPage(commentedRecipes, request);
         CustomPageResponse<CommentedRecipesResponse> response = new CustomPageResponse<>(pageResult);
         response.setContent(pageResult.getContent());
+        return response;
+    }
+
+    @Transactional(readOnly = true)
+    public CustomPageResponse<ReviewDTO> getReviews(Long recipeId, CustomPageRequest pageRequest){
+        CocktailRecipe cocktailRecipe = cocktailRecipeRepository.findById(recipeId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 레시피가 존재하지 않습니다."));
+
+        PageRequest request = PageRequest.of(pageRequest.getPage(), pageRequest.getSize());
+        Page<Review> page = reviewRepository.findReviewByCocktailRecipe(cocktailRecipe, request);
+
+        List<ReviewDTO> content = page.get().map(review -> {
+            ReviewDTO reviewDTO = new ReviewDTO(review.getId(), review.getMember().getId(), review.getStars(), review.getReview());
+            List<Taste> tastes = review.getReviewTastes().stream().map(ReviewTaste::getTaste).collect(Collectors.toList());
+            reviewDTO.setTastes(tastes);
+            return reviewDTO;
+        }).collect(Collectors.toList());
+
+        CustomPageResponse<ReviewDTO> response = new CustomPageResponse<>(page);
+        response.setContent(content);
         return response;
     }
 }
