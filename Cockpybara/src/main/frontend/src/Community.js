167f79579b7983361ca6db996041cb1557db0935
drimh <107299318+drimh@users.noreply.github.com>
2023-09-19 03:06:19 +0900

    css 수정

--- Cockpybara/src/main/frontend/src/Community.js
+++ Cockpybara/src/main/frontend/src/Community.js
@@ -1,31 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MyCommunitySection from './MyCommunitySection';
 import MonthlyPopularSection from './MonthlyPopularSection';
 import CocktailListSection from './CocktailListSection';
 import DetailHeader from './DetailHeader';
-import './Community.css'
+import './Community.css';
 
 function Community() {
-  const userId = 'user123'; // 여기에 실제 유저 아이디를 가져와서 사용하도록 변경해야 합니다.
+  const [isHeaderFixed, setIsHeaderFixed] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY > 100) {
+      setIsHeaderFixed(true);
+    } else {
+      setIsHeaderFixed(false);
+    }
+  };
+
+  window.addEventListener('scroll', handleScroll);
+
+  const userId = 'user123'; // Replace with actual user ID
 
   return (
     <div>
-      <DetailHeader/>
-        <div className="ruru">
-          <div className="community-box">
-            <div className="left-contents ">
-              <MyCommunitySection />
-              <MonthlyPopularSection />
-            </div>
-            <div className="right-contents">
-              <CocktailListSection />
-            </div>
+      <div className="ruru">
+        <div className="community-box">
+          <div className="left-contents">
+            <MyCommunitySection />
+            <MonthlyPopularSection />
+          </div>
+          <div className="right-contents">
+            <CocktailListSection />
           </div>
         </div>
+      </div>
+      <div className={`detail-header-container ${isHeaderFixed ? 'fixed' : ''}`}>
+        <DetailHeader />
+      </div>
     </div>
   );
-};
+}
 
 export default Community;
-
-
