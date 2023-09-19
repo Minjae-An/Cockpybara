2c01e0ae7d1cf4e4f2fb583aa730493a4df004c9
drimh <107299318+drimh@users.noreply.github.com>
2023-09-20 00:31:37 +0900

    pull 하기 위해서 일단 push

--- Cockpybara/src/main/frontend/src/Recipe.js
+++ Cockpybara/src/main/frontend/src/Recipe.js
@@ -511,10 +511,15 @@ const goToNextPage = () => {
       </button>
     </li>
   ))}
-  <button className="add-recipe-button" onClick={navigateToAddRecipe}>
-    레시피 추가
-  </button>
+  
 </ul>
+<div>
+<button className="add-recipe-button" onClick={navigateToAddRecipe}>
+    레시피 추가
+    {/* 링크 수정, css 수정 */}
+  </button>
+</div>
+
 <div className="pagination-container">
         <button className="pagination" onClick={goToPreviousPage} disabled={currentPage === 1}>
           이전 페이지
